fac70d0d47a1c3aa4ba598598f2f7e45a59723cc
Lucas K Allmon <lucaskallmon@gmail.com>
2020-04-05 13:53:49 -0700

    feature(firebase): move to api (#5)
    * feature(firebase): move to api
    
    * fix(api): env and some feedback

--- bot.js
+++ bot.js
@@ -1,9 +1,11 @@
+const axios = require('axios');
 const Discord = require("discord.js");
 const dotenv = require("dotenv");
-const split = require('lodash/split');
 const last = require('lodash/last');
+const replace = require('lodash/replace');
+const split = require('lodash/split');
 const trim = require('lodash/trim');
-const content = require("./acronyms.json");
+
 const client = new Discord.Client();
 dotenv.config();
 
@@ -16,21 +18,40 @@ client.on("message", msg => {
     //Don't respond to yourself, lol.
     return;
   }
-  console.log(msg.mentions.users[client.user.id]);
+
   if (msg.mentions.users.has(client.user.id)) {
-    //Only respond to text after the @			     
-    const command = trim(last(split(msg.content, '>'))).toUpperCase();
-    
+    //Reduce to just the capital letter acronym			     
+    const command = replace(trim(last(split(msg.content, '>'))), ':', '').toUpperCase();
+
     if (command === "HELP") {
-      msg.channel.send(`I know about: ${Object.keys(content)}`);
-      return;
+      msg.channel.startTyping();
+      axios
+	.get(`${process.env.ACRONYM_API_URL}.json`)
+	.then(res => {
+	  msg.channel.send(`I know about: ${Object.keys(res.data)}`);
+	})
+	.catch(err => {
+	  console.error(err.message);
+	})
+	.finally(() => {
+	  msg.channel.stopTyping();
+	})
+    } else {
+      msg.channel.startTyping();
+      axios
+	.get(`${process.env.ACRONYM_API_URL}/${command}.json`)
+	.then(res => {
+	  res.data === null ?
+	  msg.channel.send("💥 Nothin. Ask me HELP for more") :
+	  msg.channel.send(`🤔 ${res.data}?`);
+	 })
+	 .catch(err => {
+	   console.error(err.message);
+	 })
+	.finally(() => {
+	  msg.channel.stopTyping();
+	})
     }
-    if (content[command]) {
-      msg.channel.send(`🤔 ${content[command]}?`);
-      return;
-    }
-    
-    msg.channel.send("💥 Nothin. Ask me HELP for more");
   }
 });
 
