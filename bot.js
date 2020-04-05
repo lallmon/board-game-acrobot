c29ebeaa609ede7543c3276bcdc744a5d7f5d824
Lucas K Allmon <lallmon@redhat.com>
2020-04-05 00:06:56 -0700

    feature(firebase): move to api

--- bot.js
+++ bot.js
@@ -1,9 +1,9 @@
+const axios = require('axios');
 const Discord = require("discord.js");
 const dotenv = require("dotenv");
 const split = require('lodash/split');
 const last = require('lodash/last');
 const trim = require('lodash/trim');
-const content = require("./acronyms.json");
 const client = new Discord.Client();
 dotenv.config();
 
@@ -20,17 +20,26 @@ client.on("message", msg => {
   if (msg.mentions.users.has(client.user.id)) {
     //Only respond to text after the @			     
     const command = trim(last(split(msg.content, '>'))).toUpperCase();
-    
+
     if (command === "HELP") {
-      msg.channel.send(`I know about: ${Object.keys(content)}`);
-      return;
+      axios.get(`${process.env.ACRONYM_API_URL}.json`)
+	   .then(res => {
+	     msg.channel.send(`I know about: ${Object.keys(res.data)}`);
+	   })
+	   .catch(err => (
+	     console.error(err.message)
+	   ));
+    } else {  
+      axios.get(`${process.env.ACRONYM_API_URL}/${command}.json`)
+	   .then(res => {
+	     res.data === null ?
+	     msg.channel.send("💥 Nothin. Ask me HELP for more") :
+	     msg.channel.send(`🤔 ${res.data}?`);
+	   })
+	   .catch(err => {
+	     console.error(err.message)
+	   })
     }
-    if (content[command]) {
-      msg.channel.send(`🤔 ${content[command]}?`);
-      return;
-    }
-    
-    msg.channel.send("💥 Nothin. Ask me HELP for more");
   }
 });
 
